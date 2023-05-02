e23c1118b1927c466125fdd0bc55a3c13108b8b2
Jack O'Kelly <c21461876@mytudublin.ie>
2023-05-02 16:33:23 +0100

    FINAL COMMIT

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -1,5 +1,8 @@
 package ie.tudublin;
 
+// Navy Blue		Blue Grotto			Blue Green			Baby Blue
+// (5, 68, 94)		(24, 154, 180)		(117, 230, 218)		(212, 241, 244)
+
 //import example.*;
 
 import ddf.minim.AudioBuffer;
@@ -7,6 +10,9 @@ import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 import processing.core.PApplet;
+import processing.core.PVector;
+
+import java.util.ArrayList;
 
 public class Audio1 extends PApplet {
 	Minim minim;
@@ -16,10 +22,20 @@ public class Audio1 extends PApplet {
 	public Timer timer;
 
 	int mode = 0;
+	float seconds = 0;
+	float theta=0;
 
 	float y = 0;
 	float smoothedY = 0;
 	float smoothedAmplitude = 0;
+	ArrayList<Star> stars = new ArrayList<Star>();
+	float speed;
+
+	PVector[][] globe;
+	int r = 200;
+	int total = 25;
+	float angleX = 0;
+	float angleY = 0;
 
 	public void keyPressed() {
 		if (key >= '0' && key <= '9') {
@@ -33,28 +49,43 @@ public class Audio1 extends PApplet {
 				ap.play();
 			}
 		}
+		if (keyCode == '7') {
+			seconds = 0;
+		}
 	}
 
 	public void settings() {
-		//size(1024, 1024, P3D);
-		fullScreen(P3D, SPAN);
+		// size(2560 ,1600);
+		fullScreen(P3D);
 	}
 
 	public void setup() {
 		minim = new Minim(this);
-		// Uncomment this to use the microphone
-		// ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
-		// ab = ai.mix;
 
 		timer = new Timer(this);
-		// And comment the next two lines out
+		// Load Music file
 		ap = minim.loadFile("Eyelar.mp3", 1024);
 		ap.play();
 		ab = ap.mix;
-		// colorMode(HSB);
 
 		y = height / 2;
 		smoothedY = y;
+
+		for (int i = 0; i < 800; i++) {
+			stars.add(new Star());
+		}
+
+		globe = new PVector[total + 1][total + 1];
+		for (int i = 0; i < total + 1; i++) {
+			float lat = map(i, 0, total, 0, PI);
+			for (int j = 0; j < total + 1; j++) {
+				float lon = map(j, 0, total, 0, TWO_PI);
+				float x = r * sin(lat) * cos(lon);
+				float y = r * sin(lat) * sin(lon);
+				float z = r * cos(lat);
+				globe[i][j] = new PVector(x, y, z);
+			}
+		}
 
 	}
 
@@ -76,8 +107,9 @@ public class Audio1 extends PApplet {
 	}
 
 	public void draw() {
-		// background(0);
+		// timer.update();
 		float halfH = height / 2;
+		float halfW = width / 2;
 		float average = 0;
 		float sum = 0;
 		off += 1;
@@ -93,165 +125,392 @@ public class Audio1 extends PApplet {
 		float cx = width / 2;
 		float cy = height / 2;
 
-		timer.start();
-		int seconds = timer.seconds();
-		System.out.println(timer.seconds());
-		switch (mode) {
-			case (0):
-				background(0);
-				for (int i = 0; i < ab.size(); i++) {
-					// float c = map(ab.get(i), -1, 1, 0, 255);
-					float c = map(i, 0, ab.size(), 0, 255);
-					stroke(c, 255, 255);
-					float f = ab.get(i) * halfH;
-					line(i, halfH + f, i, halfH - f);
-				}
-				break;
-			case (1):
-				background(0);
-				colorMode(HSB);
-				for (int i = 0; i < ab.size(); i++) {
-					float c = map(ab.get(i), -1, 1, 0, 255);
-					// float c = map(i, 0, ab.size(), 0, 255);
-					stroke(c, 255, 255);
-					float f = ab.get(i) * halfH;
-					line(i, halfH + f, halfH - f, i);
-				}
-				break;
-			case 2:
-				background(0);
-				for (int i = 0; i < ab.size(); i++) {
+		seconds = millis() / 1000.0f;
+		System.out.println(seconds);
+		
+		//Intro
+		if (seconds >= 2 && seconds < 6) {
 
-					float c = map(i, 0, ab.size(), 0, 255);
-					stroke(c, 255, 255);
-					float f = ab.get(i) * halfH;
-					line(i, 0 + f, i, 0 - f);
-					line(0 + f, i, 0 - f, i);
-					line(height + f, i, height - f, i);
-					line(i, 1000 + f, i, 1000 - f);
-				}
-				break;
+			speed = map(50, 0, width, 0, 50);
+			background(0);
 
-			case 3:
-				background(0);
-				strokeWeight(2);
-				noFill();
-				float r = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
-				float c = map(smoothedAmplitude, 0, 0.5f, 0, 255);
-				stroke(c, 255, 255);
-				circle(cx, cy, r+(smoothedAmplitude-500));
-				circle(cx, cy, (r+150)+(smoothedAmplitude-500));
-				circle(cx, cy, (r-150)+(smoothedAmplitude-500));
-				break;
+			for (int i = 0; i < stars.size(); i++) {
+				stars.get(i).update();
+				stars.get(i).show();
+			}
 
-			case 4:
-				background(0);
-				strokeWeight(2);
-				noFill();
-				float point = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
-				float colour = map(smoothedAmplitude, 0, 0.5f, 0, 255);
-				stroke(colour, 255, 255);
-				rectMode(CENTER);
-				rect(cx, cy, point, point);
-				break;
-			case 5:
-				background(0);
-				for (int i = 0; i < ab.size(); i++) {
+			strokeWeight(1);
 
-					float color = map(i, 0, ab.size(), 0, 255);
-					stroke(color, 255, 255);
-					float f = ab.get(i) * halfH;
-					line(i, 0 + f, i, 0 - f);
-					line(0 + f, i, 0 - f, i);
-					line(height + f, i, height - f, i);
-					line(i, 1000 + f, i, 1000 - f);
+			rectMode(PApplet.CENTER);
 
-				}
-				strokeWeight(2);
-				noFill();
-				float radius = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
-				float l = map(smoothedAmplitude, 0, 0.5f, 0, 255);
-				stroke(l, 255, 255);
-				circle(cx, cy, radius);
-				break;
-			case 6:
-				// here is where the main eye is made too
+			fill(0, 0, 255);
+			textSize(200);
+			text("E", width / 2 - 350, height / 2 + 80);
 
-				colorMode(RGB);
-				background(0, 255, 0);
-
-				float halfW = width / 2;
-				halfH = height / 2;
-
-				float total = 0;
-				float amplitude = 0;
-				float smoothedAmplitude = 0;
-				for (int i = 0; i < ab.size(); i++) {
-					total += abs(ab.get(i));
-				}
-				amplitude = total / ab.size();
-				smoothedAmplitude = lerp(smoothedAmplitude, amplitude, 0.1f);
-				// make visualizer more intense
-				smoothedAmplitude = smoothedAmplitude * 3;
-
-				// make background up in intensity depending on music
-				colour = smoothedAmplitude * 1500;
-
-				// backgound colour
-				background(0, 0, 0);
-
-				// drawing the ring inside the eye
-				noFill();
-				beginShape();
-				for (float i = 0; i < TWO_PI; i += 0.01f) {
-					stroke(0);
-					strokeWeight(15);
-					float r1 = width / 2.5f;
-					float x1 = r1 * cos(i);
-					float y1 = r1 * pow(sin(i), 3) * 0.5f;
-					// puts the eye in the center of the screen
-					vertex(x1 + halfW, y1 + halfH);
-
-					stroke(84, 143, 168);
-					strokeWeight(5);
-					// make the ring go crazy and randomly but still on sync to music
-					r = width / 9 + (smoothedAmplitude * random(10, 150) * 2);
-					float x = r * cos(i);
-					float y = r * sin(i);
-					// center the ring
-					vertex(x + halfW, y + halfH);
-				}
-				endShape(CLOSE);
-
-				// create the iris
-				strokeWeight(10);
-				// float colors = map(smoothedAmplitude, 0, 0.5f, 0, 255);
-				for (int i = 0; i < ab.size(); i++) {
-					// the iris
-					radius = map(smoothedAmplitude, 0, 0.6f, width / 4, 500);
-					fill(255, 255, 255);
-					circle(halfW, halfH, radius);
-
-					// draw the pupil for the eye
-					float radius2 = map(smoothedAmplitude * 1.5f, 0, 0.7f, width / 20, 500);
-					fill(0);
-					circle(halfW, halfH, radius2+125);
-
-					fill(0,0,255);
-					circle(halfW, halfH, smoothedAmplitude);
-				}
-				// drawing 3 random stars
-				strokeWeight(2);
-				for (int i = 0; i < 6; i++) {
-					fill(0);
-					stroke(0); 
-					//circle(random(width), random(height), random(width));
-					star(random(width),random(height),10,19,5);
-					delay(10);
-				}
-
-				break;
 		}
 
+		else if (seconds >= 6 && seconds < 9) {
+
+			speed = map(50, 0, width, 0, 50);
+			background(0);
+
+			for (int i = 0; i < stars.size(); i++) {
+				stars.get(i).update();
+				stars.get(i).show();
+			}
+
+			strokeWeight(1);
+
+			rectMode(PApplet.CENTER);
+
+			fill(5, 92, 242);
+			textSize(200);
+			text("EY", width / 2 - 350, height / 2 + 80);
+
+		}
+
+		else if (seconds >= 9 && seconds < 12) {
+
+			speed = map(50, 0, width, 0, 50);
+			background(0);
+
+			for (int i = 0; i < stars.size(); i++) {
+				stars.get(i).update();
+				stars.get(i).show();
+			}
+
+			strokeWeight(1);
+
+			rectMode(PApplet.CENTER);
+
+			fill(12, 71, 173);
+			textSize(200);
+			text("EYE", width / 2 - 350, height / 2 + 80);
+
+		}
+
+		else if (seconds >= 12 && seconds < 15) {
+
+			speed = map(50, 0, width, 0, 50);
+			background(0);
+
+			for (int i = 0; i < stars.size(); i++) {
+				stars.get(i).update();
+				stars.get(i).show();
+			}
+
+			strokeWeight(1);
+
+			rectMode(PApplet.CENTER);
+			fill(59, 127, 245);
+			textSize(200);
+			text("EYEL", width / 2 - 350, height / 2 + 80);
+
+		}
+
+		else if (seconds >= 15 && seconds < 18) {
+
+			speed = map(50, 0, width, 0, 50);
+			background(0);
+
+			for (int i = 0; i < stars.size(); i++) {
+				stars.get(i).update();
+				stars.get(i).show();
+			}
+
+			strokeWeight(1);
+
+			rectMode(PApplet.CENTER);
+			fill(14, 52, 117);
+			textSize(200);
+			text("EYELA", width / 2 - 350, height / 2 + 80);
+
+		}
+		//End Intro
+		else if (seconds >= 18 && seconds < 20) {
+
+			speed = map(50, 0, width, 0, 50);
+			background(0);
+			for (int i = 0; i < stars.size(); i++) {
+				stars.get(i).update();
+				stars.get(i).show();
+			}
+
+			strokeWeight(1);
+
+			rectMode(PApplet.CENTER);
+			fill(12, 98, 245);
+			textSize(200);
+			text("EYELAR", width / 2 - 350, height / 2 + 80);
+
+			
+			//Solar System
+		} else if (seconds >= 20 && seconds < 35) {
+			float r = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
+			background(0);
+			stroke(0);
+			speed = map(80, 0, width, 0, 50);
+
+			for (int i = 0; i < stars.size(); i++) {
+				stars.get(i).update();
+				stars.get(i).show();
+			}
+
+			// Translate to center of window to draw the sun.
+			translate(width / 2, height / 2);
+			fill(84, 143, 168);
+			ellipse(0, 0, 320, 320);
+			fill(0);
+			ellipse(0, 0,((r + 150) + (smoothedAmplitude - 500))/2,((r + 150) + (smoothedAmplitude - 500))/2);
+
+			// The earth rotates around the sun
+			pushMatrix();
+			rotate(theta);
+			translate(250, 0);
+			fill(5, 68, 94);
+			ellipse(0, 0, 160, 160);
+			
+			// Moon #1 rotates around the earth
+			// pushMatrix() is called to save the transformation state before drawing moon
+			// #1.
+			// This way we can pop and return to earth before drawing moon #2.
+			// Both moons rotate around the earth (which itself is rotating around the sun).
+			//pushMatrix();
+			rotate(-theta * 4);
+			translate(150, 0);
+			fill(24, 154, 180);
+			ellipse(0, 0, 60, 60);
+			//popMatrix();
+
+			// Moon #2 also rotates around the earth
+			//pushMatrix();
+			rotate(theta * 2);
+			translate(100, 0);
+			fill(212, 241, 244);
+			ellipse(0, 0, 30, 30);
+			//popMatrix();
+
+			//pushMatrix();
+			rotate(-theta * 4);
+			translate(230, 0);
+			fill(50, 255, 200);
+			ellipse(0, 0, 45, 45);
+			//popMatrix();
+
+			//pushMatrix();
+			rotate(-theta * 4);
+			translate(285, 0);
+			fill(117, 230, 218);
+			ellipse(0, 0, 90, 90);
+			//popMatrix();
+
+			popMatrix();
+
+			theta += 0.01;
+
+		}// else if (seconds >= 25 && seconds < 35) {
+
+		//}
+
+		// Horizon vertex animation
+		else if (seconds >= 35 && seconds < 45) {
+			background(0);
+			strokeWeight(2);
+			noFill();
+			beginShape();
+			for (float i = 0; i < TWO_PI; i += 0.01f) {
+				stroke(0);
+				strokeWeight(15);
+				float r1 = width / 1.5f;
+				float x2 = r1 * cos(i);
+				float y1 = r1 * pow(sin(i), 3) * 0.5f;
+				// puts the eye in the center of the screen
+				vertex(x2 + cx, y1 + halfH);
+
+				stroke(84, 143, 168);
+				strokeWeight(5);
+				// make the ring go crazy and randomly but still on sync to music
+				float r2 = width / 9 + (smoothedAmplitude * random(10, 150) * 2);
+				float x = r2 * cos(i);
+				// center the ring
+				vertex(x + cx, 50 + halfH);
+			}
+			endShape(CLOSE);
+
+			float r = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
+			stroke(84, 143, 168);
+			circle(cx, cy, r + (smoothedAmplitude - 500));
+			circle(cx, cy, (r + 150) + (smoothedAmplitude - 500));
+			circle(cx, cy, (r - 150) + (smoothedAmplitude - 500));
+
+		}
+
+		else if (seconds >= 45 && seconds < 48) {
+			background(0);
+		}
+
+		// eye animation
+		else if (seconds >= 48 && seconds < 50) {
+			// here is where the main eye is made too
+
+			colorMode(RGB);
+			background(0, 255, 0);
+
+			halfH = height / 2;
+
+			float total = 0;
+			float amplitude = 0;
+			float smoothedAmplitude = 0;
+			for (int i = 0; i < ab.size(); i++) {
+				total += abs(ab.get(i));
+			}
+			amplitude = total / ab.size();
+			smoothedAmplitude = lerp(smoothedAmplitude, amplitude, 0.1f);
+			// make visualizer more intense
+			smoothedAmplitude = smoothedAmplitude * 3;
+
+			// backgound colour
+			background(0, 0, 0);
+
+			// drawing the ring inside the eye
+			noFill();
+			beginShape();
+			for (float i = 0; i < TWO_PI; i += 0.01f) {
+				stroke(0);
+				strokeWeight(15);
+				float r1 = width / 2.5f;
+				float x2 = r1 * cos(i);
+				float y1 = r1 * pow(sin(i), 3) * 0.5f;
+				// puts the eye in the center of the screen
+				vertex(x2 + halfW, y1 + halfH);
+
+				stroke(84, 143, 168);
+				strokeWeight(5);
+				// make the ring go crazy and randomly but still on sync to music
+				float r = width / 9 + (smoothedAmplitude * random(10, 150) * 2);
+				float x = r * cos(i);
+				float y = r * sin(i);
+				// center the ring
+				vertex(x + halfW, y + halfH);
+			}
+			endShape(CLOSE);
+
+			// create the iris
+			strokeWeight(10);
+			// the iris
+			float radius = map(smoothedAmplitude, 0, 0.6f, width / 4, 500);
+			fill(255, 255, 255);
+			circle(halfW, halfH, radius);
+
+			// draw the pupil for the eye
+			float radius2 = map(smoothedAmplitude * 1.5f, 0, 0.7f, width / 20, 500);
+			fill(0);
+			circle(halfW, halfH, radius2 + 125);
+
+			fill(0, 0, 255);
+			circle(halfW, halfH, smoothedAmplitude);
+			// drawing 3 random stars
+			strokeWeight(2);
+			for (int i = 0; i < 6; i++) {
+				fill(0);
+				stroke(0);
+				star(random(width), random(height), 10, 19, 5);
+				delay(10);
+			}
+		}
+
+		else if (seconds >= 50 && seconds >= 60){
+			background(0);
+			strokeWeight(2);
+			noFill();
+			beginShape();
+			for (int i = 0; i < stars.size(); i++) {
+				stars.get(i).update();
+				stars.get(i).show();
+			}
+			for (float i = 0; i < TWO_PI; i += 0.01f) {
+				stroke(0);
+				strokeWeight(15);
+				float r1 = width / 1.5f;
+				float x2 = r1 * cos(i);
+				float y1 = r1 * pow(sin(i), 3) * 0.2f;
+				// puts the eye in the center of the screen
+				vertex(x2 + cx, y1 + halfH);
+
+				stroke(84, 143, 168);
+				strokeWeight(5);
+				// make the ring go crazy and randomly but still on sync to music
+				float r2 = width / 9 + (smoothedAmplitude * random(5, 100) * 2);
+				float x = r2 * cos(i);
+				// center the ring
+				vertex(x + cx, 50 + halfH);
+			}
+			endShape(CLOSE);
+
+			float r = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
+			stroke(84, 143, 168);
+			//circle(cx, cy, r + (smoothedAmplitude - 500));
+			//circle(cx, cy, (r + 150) + (smoothedAmplitude - 500));
+			//circle(cx, cy, (r - 150) + (smoothedAmplitude - 500));
+		
+		}
+
+		else if (seconds >= 60 && seconds >= 62){
+			background(0);
+		}
+
+		else if (seconds >= 63 && seconds >= 67){
+			background(0);
+
+			for (int i = 0; i < stars.size(); i++) {
+				stars.get(i).update();
+				stars.get(i).show();
+			}
+		}
+
+
+	}
+
+	class Star {
+		float x;
+		float y;
+		float z;
+		float pz;
+
+		Star() {
+			x = random(-width / 2, width / 2);
+			y = random(-height / 2, height / 2);
+			z = random(width / 2);
+			pz = z;
+		}
+
+		void update() {
+			z = z - speed;
+			if (z < 1) {
+				z = width / 2;
+				x = random(-width / 2, width / 2);
+				y = random(-height / 2, height / 2);
+				pz = z;
+			}
+		}
+
+		void show() {
+			fill(255);
+			noStroke();
+
+			float sx = map(x / z, 0, 1, 0, width);
+			float sy = map(y / z, 0, 1, 0, height);
+
+			float r = map(z, 0, width / 2, 8, 0);
+			ellipse(sx, sy, r, r);
+
+			float px = map(x / pz, 0, 1, 0, width);
+			float py = map(y / pz, 0, 1, 0, height);
+
+			stroke(255);
+			line(px, py, sx, sy);
+
+			pz = z;
+		}
 	}
 }
